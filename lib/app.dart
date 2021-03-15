9f49dcb8dd8d317b24bebb6f387146494f1c2a03
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2021-03-15 14:07:50 +0100

    basic structure

---
+++ lib/app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:jedzonko/view/menu.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Menu(),
+    );
+  }
+}
---
+++ lib/view/menu.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Menu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Jedzonko'),
+      ),
+      body: Container(),
+    );
+  }
+}
