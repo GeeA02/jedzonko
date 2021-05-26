7c5493fbf6d49040e265133963634a43ddbe6f65
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2021-05-26 11:26:55 +0200

    Created product in model

---
+++ lib/model/product.dart
@@ -0,0 +1,8 @@
+class Product {
+  String barcode;
+  String name;
+  String imageUrl;
+  String date;
+
+  Product(this.barcode, this.name, this.imageUrl, this.date);
+}
--- lib/viewModel/historyViewModel.dart
+++ lib/viewModel/historyViewModel.dart
@@ -1,26 +1,22 @@
+import 'package:jedzonko/model/product.dart';
+
 class HistoryViewModel {
   List<Product> tmpProducts;
 
   HistoryViewModel() {
     tmpProducts = [
       Product(
+          null,
           "Mleko",
           "https://static.openfoodfacts.org/images/products/590/082/000/0011/front_pl.39.400.jpg",
           DateTime.now().toIso8601String()),
-      Product("Chleb", null, DateTime.now().toIso8601String()),
+      Product(null, "Chleb", null, DateTime.now().toIso8601String()),
       Product(
+          null,
           "Kawa",
           "https://static.openfoodfacts.org/images/products/590/115/404/2746/front_pl.4.400.jpg",
           DateTime.now().toIso8601String()),
-      Product("Majonez", null, DateTime.now().toIso8601String())
+      Product(null, "Majonez", null, DateTime.now().toIso8601String())
     ];
   }
-}
-
-class Product {
-  String name;
-  String imageUrl;
-  String date;
-
-  Product(this.name, this.imageUrl, this.date);
 }
