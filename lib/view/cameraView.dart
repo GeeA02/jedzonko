7d33df5ce2c8ac21d5d1916d0cc2d166416dfee6
GeeA02 <43759867+GeeA02@users.noreply.github.com>
2021-06-23 14:35:58 +0200

    place camer in stateful widget

--- lib/view/cameraView.dart
+++ lib/view/cameraView.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jedzonko/api/api.dart';
 import 'package:jedzonko/view/productView.dart';
+import 'package:jedzonko/view/widgets/cameraWidget.dart';
 import 'package:jedzonko/view/widgets/loadingDialog.dart';
 import 'package:qr_mobile_vision/qr_camera.dart';
 
@@ -19,50 +20,7 @@ class CameraView extends StatelessWidget {
             mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              //TODO  v make this responsible v
-              SizedBox(
-                width: 300.0,
-                height: 250.0,
-                child: QrCamera(
-                  notStartedBuilder: (context) => Center(
-                      child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(
-                        Theme.of(context).primaryColor),
-                  )),
-                  onError: (context, error) =>
-                      Center(child: Text(error.toString())),
-                  qrCodeCallback: (String? barcode) {
-                    //TODO save result in localDB and show productView or errorProductView
-                    print(barcode);
-
-                    // version 1
-                    // showDialog<void>(
-                    //   context: context,
-                    //   builder: (context) => LoadingDialog(barcode!),
-                    // );
-
-                    // //version 2
-                    // API.fetchApiProduct(barcode!).then((value) =>
-                    //     Navigator.pushNamed(context, ProductView.routeName,
-                    //         arguments: ProductScreenArguments(
-                    //             value.product, value.nutriments)));
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      border: Border.symmetric(
-                          vertical: BorderSide(
-                            color: Colors.grey.withOpacity(0.5),
-                            width: 35.0,
-                          ),
-                          horizontal: BorderSide(
-                            color: Colors.grey.withOpacity(0.5),
-                            width: 60.0,
-                          )),
-                    ),
-                  ),
-                ),
-              )
+              CameraWidget(),
             ]),
       ),
     )));
---
+++ lib/view/widgets/cameraWidget.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_mobile_vision/qr_camera.dart';
+import 'package:qr_mobile_vision/qr_mobile_vision.dart';
+
+class CameraWidget extends StatefulWidget {
+  @override
+  _CameraWidgetState createState() => _CameraWidgetState();
+}
+
+class _CameraWidgetState extends State<CameraWidget> {
+  bool _camState = true;
+  @override
+  void dispose() {
+    super.dispose();
+    _camState = false;
+    print('nałra');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width - 20,
+      height: MediaQuery.of(context).size.height - 700,
+      child: _camState
+          ? QrCamera(
+              formats: [
+                BarcodeFormats.CODE_128,
+                BarcodeFormats.CODE_39,
+                BarcodeFormats.CODE_93,
+                BarcodeFormats.CODABAR,
+              ],
+              notStartedBuilder: (context) => Center(
+                  child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(
+                    Theme.of(context).primaryColor),
+              )),
+              onError: (context, error) =>
+                  Center(child: Text(error.toString())),
+              qrCodeCallback: (String? barcode) {
+                //TODO save result in localDB and show productView or errorProductView
+                print(barcode);
+                setState(() {
+                  _camState = false;
+                });
+                // version 1
+                // showDialog<void>(
+                //   context: context,
+                //   builder: (context) => LoadingDialog(barcode!),
+                // );
+
+                // //version 2
+                // API.fetchApiProduct(barcode!).then((value) =>
+                //     Navigator.pushNamed(context, ProductView.routeName,
+                //         arguments: ProductScreenArguments(
+                //             value.product, value.nutriments)));
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  border: Border.symmetric(
+                      vertical: BorderSide(
+                        color: Colors.grey.withOpacity(0.5),
+                        width: 35.0,
+                      ),
+                      horizontal: BorderSide(
+                        color: Colors.grey.withOpacity(0.5),
+                        width: 60.0,
+                      )),
+                ),
+              ),
+            )
+          : Center(child: Text("Camera inactive")),
+    );
+  }
+}
